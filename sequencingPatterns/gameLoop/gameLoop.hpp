79bbef9bcd690ea69ffdf9018eb261fc9e8d79aa
Dominic Forster <dominicforster@gmx.de>
2017-04-05 06:25:54 +0200

    update

---
+++ sequencingPatterns/gameLoop/gameLoop.hpp
@@ -0,0 +1,20 @@
+double previous = getCurrentTime();
+double lag = 0.0;
+
+while (true)
+{
+  double current = getCurrentTime();
+  double elapsed = current - previous;
+  previous = current;
+  lag += elapsed;
+
+  processInput();
+
+  while (lag >= MS_PER_UPDATE)
+  {
+    update();
+    lag -= MS_PER_UPDATE;
+  }
+
+  render();
+}
